ea4d54d5368c51cb0e77f65e3b701e09492a6f25
Amanda Grice <algrice@bu.edu>
2021-01-02 14:53:35 -0500

    add simple graphics

--- c4.py
+++ c4.py
@@ -1,10 +1,42 @@
 import numpy as np
+import pygame
+import math
 
 rows = 6
 cols = 7
 board = np.zeros((rows, cols))
 game_over = False
 turn = 0
+
+slot_width = 100
+board_width = cols * slot_width
+board_height = (rows + 1) * slot_width
+board_size = (board_width, board_height)
+
+red = (255, 0, 0)
+yellow = (255, 255, 0)
+blue = (0, 0, 255)
+white = (0, 0, 0)
+offset = 100
+radius = int(slot_width / 2 - 5)
+
+def draw_board(board):
+	print("called draw board")
+	pygame.draw.rect(window, white, (0, 0, board_width, slot_width))
+	for c in range(cols):
+		for r in range(rows):
+			rect = (c * slot_width, r * slot_width + offset, slot_width, slot_width)
+			c1 = (int(c * slot_width + slot_width / 2), int(r * slot_width + offset + slot_width / 2))
+			pygame.draw.rect(window, blue, rect)
+			pygame.draw.circle(window, white, c1, radius)
+	for c in range(cols):
+		for r in range(rows):
+			c2 = (int(c * slot_width + slot_width / 2), board_height - int(r * slot_width + slot_width / 2))
+			if board[r][c] == 1:
+				pygame.draw.circle(window, red, c2, radius)
+			elif board[r][c] == 2:
+				pygame.draw.circle(window, yellow, c2, radius)
+	pygame.display.update()
 
 def is_valid_column(board, col):
 	return board[rows - 1][col] == 0
@@ -16,6 +48,7 @@ def drop_piece(board, col, piece):
 			row = r
 			break
 	board[row][col] = piece
+	draw_board(board)
 
 def is_win_move(board, piece):
 	# horizontal check
@@ -39,27 +72,39 @@ def is_win_move(board, piece):
 				return True
 
 
+pygame.init()
+window = pygame.display.set_mode(board_size)
+pygame.display.set_caption("Connect Four")
+draw_board(board)
+
 while not game_over:
-	if turn % 2 == 0:
-		# player 1 turn
-		col = int(input("Player 1 Please Pick a Column: "))
-		if (is_valid_column(board, col)):
-			drop_piece(board, col - 1, 1)
-			if (is_win_move(board, 1)):
-				game_over = True
-				print("Player 1 Wins!")
-		else: 
-			turn -= 1
-	else:
-		# player 2 turn
-		col = int(input("Player 2 Please Pick a Column: "))
-		if (is_valid_column(board, col)):
-			drop_piece(board, col - 1, 2)
-			if (is_win_move(board, 2)):
-				game_over = True
-				print("Player 2 Wins!")
-		else: 
-			turn -= 1
-	print(np.flip(board, 0))
-	print()
-	turn += 1
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			quit()
+		if event.type == pygame.MOUSEBUTTONDOWN:	
+			print(event.pos)
+			posx = event.pos[0]
+			if turn % 2 == 0:
+				# player 1 turn
+				col = math.floor(posx / slot_width)
+				if (is_valid_column(board, col)):
+					drop_piece(board, col, 1)
+					if (is_win_move(board, 1)):
+						game_over = True
+						print("Player 1 Wins!")
+				else: 
+					turn -= 1
+			else:
+				# player 2 turn
+				col = math.floor(posx / slot_width)
+				if (is_valid_column(board, col)):
+					drop_piece(board, col, 2)
+					if (is_win_move(board, 2)):
+						game_over = True
+						print("Player 2 Wins!")
+				else: 
+					turn -= 1
+			print(np.flip(board, 0))
+			print()
+			turn += 1
